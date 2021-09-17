19b2e3e12c96e316da7bc1383a239e0505518636
JPaulSoriano <johnpaul.soriano.6197@gmail.com>
2021-09-17 10:30:09 +0800

    links

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -9,7 +9,7 @@ class PublicController extends Controller
 {
     public function index()
     {
-        $latestposts = Post::where('featured', '=', '0')->limit(3)->get()->sortDesc();
+        $latestposts = Post::where('featured', '=', '0')->orderBy('created_at', 'desc')->limit(3)->get();
         $featuredposts = Post::where('featured', '=', '1')->limit(3)->get();
         $categories = Category::all();
         $moreposts = Post::inRandomOrder()->limit(10)->get();
